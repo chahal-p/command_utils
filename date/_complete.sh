ec37dfb8d6e5df1043ef51dff7ec2f6dafd8cad8
Parveen Chahal <hello@parveen.me>
2026-02-22 15:05:30 -0800

    Refactoring

--- date/_complete.sh
+++ date/_complete.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+function _complete_cu.date() {
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=( $(compgen -W "--out_utc --enable_fraction -h --help" -- "$cur") )
+}
+complete -F _complete_cu.date cu.date
+
 function _complete_cu.date.from-epoch() {
   local cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -W "--out_utc --nanoseconds --microseconds --milliseconds --seconds" -- "$cur") )
+  COMPREPLY=( $(compgen -W "--out_utc --nanoseconds --microseconds --milliseconds --seconds --enable_fraction" -- "$cur") )
 }
 complete -F _complete_cu.date.from-epoch cu.date.from-epoch
 
