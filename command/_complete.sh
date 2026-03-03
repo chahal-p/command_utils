9ee53138dbc1b36e6485c60d9484180ca84c1380
Parveen Chahal <hello@parveen.me>
2026-03-03 01:40:37 -0800

    Refactoring

--- command/_complete.sh
+++ command/_complete.sh
@@ -2,6 +2,6 @@
 
 function _complete_cu.command() {
   local cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -f -W "-c --confirm -v --verbose --lock_file -h --help" -- "$cur") )
+  COMPREPLY=( $(compgen -f -W "-c --confirm -v --verbose -t --timeout -s --signal -k --kill_after --lock_file --lock_timeout -h --help" -- "$cur") )
 }
 complete -F _complete_cu.command cu.command
