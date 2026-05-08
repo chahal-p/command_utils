d0e0a4e959e29590cf77d87bce340a6549008f87
Parveen Chahal <hello@parveen.me>
2026-05-08 13:51:40 -0700

    Update _complete.sh

--- command/_complete.sh
+++ command/_complete.sh
@@ -2,6 +2,6 @@
 
 function _complete_cu.command() {
   local cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -f -W "-c --confirm -v --verbose -t --timeout -s --signal -k --kill_after --lock_file --lock_timeout -h --help" -- "$cur") )
+  COMPREPLY=( $(compgen -f -W "-c --confirm -v --verbose -l --line -t --timeout -s --signal -k --kill_after --lock_file --nonblock -h --help" -- "$cur") )
 }
 complete -F _complete_cu.command cu.command
