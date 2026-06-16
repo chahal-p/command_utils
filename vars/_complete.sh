90327fc7aa85dd392ed6888e125d521ba0a7da8d
Parveen Chahal <hello@parveen.me>
2026-06-15 21:05:45 -0700

    Refactor completion functions for cu.vars

--- vars/_complete.sh
+++ vars/_complete.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-function _complete_cu.vars.create() {
-  local cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -W "--var" -- "$cur") )
+_complete_cu.vars() {
+  COMPREPLY=()
+  local cmd="${COMP_WORDS[0]}"
+  local second_arg="${COMP_WORDS[1]}"
+  local cur_word="${COMP_WORDS[COMP_CWORD]}"
+  local prev_word="${COMP_WORDS[COMP_CWORD-1]}"
+  if [[ "$prev_word" == "$cmd" ]] ; then
+    if [[ $cur_word == "-"* ]] ; then
+      COMPREPLY=( $(compgen -W "-d --delete --envs -h --help" -- "$cur_word") )
+    else
+      COMPREPLY=( $(compgen -W "$(cu.vars | xargs)" -- "$cur_word") )
+    fi
+  elif [[ "$prev_word" == "--delete" || "$prev_word" == "-d" ]] ; then
+    COMPREPLY=( $(compgen -W "$(cu.vars | xargs)" -- "$cur_word") )
+  elif [[ "$prev_word" == "--show" ]] ; then
+    COMPREPLY=( $(compgen -W "$(cu.vars | xargs)" -- "$cur_word") )
+  elif [[ ! $second_arg == "-"* ]]; then
+    COMPREPLY=( $(compgen -f -d -- "$cur_word") )
+  fi
 }
-complete -F _complete_cu.vars.create cu.vars.create
-
-function _complete_cu.vars.delete() {
-  local cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -W "--var" -- "$cur") )
-}
-complete -F _complete_cu.vars.delete cu.vars.delete
+complete -F _complete_cu.vars cu.vars
